84fc3aa0ef6394a4b2677ae2d5f1f28123227df8
thet-beyondlimits <thet@beyondlimitsmm.com>
2023-05-04 16:52:05 +0630

    Add Google Map

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,10 @@
-import { FiFacebook, FiGithub, FiInstagram, FiTwitter } from "react-icons/fi";
+import {
+  FiFacebook,
+  FiGithub,
+  FiInstagram,
+  FiLinkedin,
+  FiTwitter,
+} from "react-icons/fi";
 import Logo from "/tab_logo.png";
 
 export const Footer = () => {
@@ -102,11 +108,14 @@ export const Footer = () => {
         </div>
 
         <div className="hidden justify-end gap-3 lg:flex">
-          <SocialCircle icon={<FiTwitter size={20} />} link=""></SocialCircle>
+          <SocialCircle
+            icon={<FiLinkedin size={20} />}
+            link="https://www.linkedin.com/company/beyond-limits-technologies-myanmar/"
+          ></SocialCircle>
           <SocialCircle
             icon={<FiFacebook size={20} />}
             customStyle="bg-primary text-white border-primary"
-            link=""
+            link="https://www.facebook.com/beyondlimitsmm"
           ></SocialCircle>
           <SocialCircle icon={<FiInstagram size={20} />} link=""></SocialCircle>
           <SocialCircle icon={<FiGithub size={20} />} link=""></SocialCircle>
@@ -116,11 +125,14 @@ export const Footer = () => {
       <div className="flex flex-col-reverse justify-between gap-4 md:flex-row lg:justify-center">
         <div>© Copyright 2023, All Rights Reserved by Beyond Limits</div>
         <div className="flex gap-3 lg:hidden">
-          <SocialCircle icon={<FiTwitter size={20} />} link=""></SocialCircle>
+          <SocialCircle
+            icon={<FiLinkedin size={20} />}
+            link="https://www.linkedin.com/company/beyond-limits-technologies-myanmar/"
+          ></SocialCircle>
           <SocialCircle
             icon={<FiFacebook size={20} />}
             customStyle="bg-primary text-white border-primary"
-            link=""
+            link="https://www.facebook.com/beyondlimitsmm"
           ></SocialCircle>
           <SocialCircle icon={<FiInstagram size={20} />} link=""></SocialCircle>
           <SocialCircle icon={<FiGithub size={20} />} link=""></SocialCircle>
@@ -129,87 +141,6 @@ export const Footer = () => {
     </footer>
   );
 };
-
-// export const Footer = () => {
-//   return (
-//     <footer className="Footer container mx-6 mb-12 sm:mx-auto lg:mb-24 ">
-//       <div className="flex flex-col justify-center gap-6 md:flex-row md:gap-0">
-// <div className="md:w-1/2 lg:w-1/4">
-//   <div className="mb-3 flex items-center">
-//     <img
-//       src={Logo}
-//       alt=""
-//       width={32}
-//       height={32}
-//       className="overflow-hidden rounded-lg"
-//     />
-//     <h5 className="ml-2 text-primary">Beyond Limits</h5>
-//   </div>
-//   <p className="max-w-[250px] text-text-secondary45">
-//     We are top rated Digital Marketing and Software Provider in Myanmar.
-//   </p>
-// </div>
-//         <div className="row md:w-1/2 lg:w-1/3">
-//           <li className="flex w-20 items-center lg:w-24">
-//             <a className="" href="#home">
-//               Home
-//             </a>
-//           </li>
-//           <li className="flex w-20 items-center lg:w-24">
-//             <a className="" href="#aboutUs">
-//               About Us
-//             </a>
-//           </li>
-//           <li className="flex w-20 items-center lg:w-24">
-//             <a className="" href="#services">
-//               Services
-//             </a>
-//           </li>
-//           <li className="flex w-20 items-center lg:w-24">
-//             <a className="" href="#process">
-//               Process
-//             </a>
-//           </li>
-//           <li className="flex w-20 items-center lg:w-24">
-//             <a className="" href="#team">
-//               Team
-//             </a>
-//           </li>
-//           <li className="flex w-20 items-center lg:w-24">
-//             <a className="" href="#whyUs">
-//               Why Us
-//             </a>
-//           </li>
-//           <li className="flex w-20 items-center lg:w-24">
-//             <a className="" href="#contactUs">
-//               Contact Us
-//             </a>
-//           </li>
-//         </div>
-//         <div className="flex gap-3 md:hidden lg:flex lg:w-1/4">
-//           <SocialCircle icon={<FiTwitter />} link=""></SocialCircle>
-//           <SocialCircle
-//             icon={<FiFacebook />}
-//             customStyle="bg-primary text-white border-primary"
-//             link=""
-//           ></SocialCircle>
-//           <SocialCircle icon={<FiInstagram />} link=""></SocialCircle>
-//           <SocialCircle icon={<FiGithub />} link=""></SocialCircle>
-//         </div>
-//       </div>
-//       <div className="mt-4 hidden gap-3 md:flex md:w-1/4 lg:hidden">
-// <SocialCircle icon={<FiTwitter />} link=""></SocialCircle>
-// <SocialCircle
-//   icon={<FiFacebook />}
-//   customStyle="bg-primary text-white border-primary"
-//   link=""
-// ></SocialCircle>
-// <SocialCircle icon={<FiInstagram />} link=""></SocialCircle>
-// <SocialCircle icon={<FiGithub />} link=""></SocialCircle>
-//       </div>
-//     </footer>
-//   );
-// };
 
 export const SocialCircle = ({
   icon,
@@ -226,7 +157,7 @@ export const SocialCircle = ({
         customStyle === undefined ? " border-text-secondary45" : customStyle
       }`}
     >
-      <a href={link} className="">
+      <a href={link} target="_blank" className="">
         {icon}
       </a>
     </div>
--- src/section/ContactSection.tsx
+++ src/section/ContactSection.tsx
@@ -8,10 +8,19 @@ export const ContactSection = () => {
       id="contactUs"
       className="container flex flex-col items-center justify-between gap-6 py-10 lg:flex-row "
     >
-      <div className="h-56 rounded-xl md:h-80 lg:h-96 lg:w-1/2">
-        <img src={Map} alt="" className="h-full" />
+      <div className="h-64 rounded-xl md:h-80 lg:h-96 lg:w-1/2">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d954.7671578996177!2d96.131527!3d16.822951!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30c195783c572807%3A0xd0668fc37bbad07!2sDiamond%20Condominium!5e0!3m2!1sen!2smm!4v1683195660940!5m2!1sen!2smm"
+          width="1000"
+          height="450"
+          style={{ border: 0 }}
+          allowFullScreen={false}
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="h-full max-w-[360px] md:max-w-[542px] lg:max-w-full"
+        ></iframe>
       </div>
-      <div className="ContactInfo relative flex h-72 max-w-[378px] flex-col justify-center gap-8 overflow-hidden rounded-xl bg-primary px-8 md:h-80 md:w-4/5 md:max-w-[542px] lg:h-96 lg:w-1/2 lg:max-w-full">
+      <div className="ContactInfo relative flex h-72 max-w-[360px] flex-col justify-center gap-8 overflow-hidden rounded-xl bg-primary px-8 md:h-80 md:w-4/5 md:max-w-[542px] lg:h-96 lg:w-1/2 lg:max-w-full">
         <img
           src={Contact1}
           alt=""
